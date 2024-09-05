2a3bcbec516fe770e57898fdbcaedffaf3fb2fa2
Sainathhiwale <sainathhiwale2@gmail.com>
2024-09-05 20:51:34 +0530

    field injection dependency is implemented

--- app/src/main/java/com/examen/dagger2_kotlin/MainActivity.kt
+++ app/src/main/java/com/examen/dagger2_kotlin/MainActivity.kt
@@ -3,18 +3,26 @@ package com.examen.dagger2_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.examen.dagger2_kotlin.databinding.ActivityMainBinding
+import javax.inject.Inject
 
 // Field Injection Dependency
 class MainActivity : AppCompatActivity() {
-     val component = DaggerUserRegisterationComponent.builder().build()
+     @Inject
+     lateinit var userRegistrationService: UserRegistrationService
+     // consumer request dependency to component
+     @Inject
+     lateinit var  emailService: EmailService
+
+
     private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
-        val userRegistrationService = component.getUserRegisterationService()
+        val component = DaggerUserRegisterationComponent.builder().build()
+        component.injectMainActivity(this)
         userRegistrationService.registerUser("dummy@gmail.com","123456")
-        val emailService = component.getEmailService()
-         emailService.sent("john@gmail.com", "dummy@gmail.com","dummy")
+        emailService.sent("john@gmail.com","dummy@gmail.com","dummy")
+
     }
 }
