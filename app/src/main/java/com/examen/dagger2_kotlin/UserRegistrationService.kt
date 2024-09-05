7c302abfda3c16b77305fbdcee7ee13f2fe0ec31
Sainathhiwale <sainathhiwale2@gmail.com>
2024-09-05 19:47:36 +0530

    delete the class

--- app/src/main/java/com/examen/dagger2_kotlin/UserRegistrationService.kt
+++ app/src/main/java/com/examen/dagger2_kotlin/UserRegistrationService.kt
@@ -1,6 +1,8 @@
 package com.examen.dagger2_kotlin
 
-class UserRegistrationService (private val userRepository:UserRepository, private  val emailService:EmailService){
+
+class UserRegistrationService(private val userRepository:UserRepository,
+                                                  private  val emailService:EmailService){
 
     fun registerUser(email:String,password:String){
         userRepository.saveUser(email,password)
